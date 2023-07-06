35c0d704ff0037bddfba3efb09bcd3c15ee9d3c0
Janiya_X <95123183+Janithsadanuwan@users.noreply.github.com>
2023-07-06 19:22:46 +0530

    `Queen Nilu 2.0v Beta`

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env'
 
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
-}
+} 
 
 /*
 TO GET PASSWORD WATCH THIS 
