261585478e6e5bf3441e366f78be67b5e3927e1e
Janiya_X <95123183+Janithsadanuwan@users.noreply.github.com>
2023-07-06 19:17:46 +0530

    `Queen Nilu 2.0 v Beta`

--- replit.nix
+++ replit.nix
@@ -1,9 +1,20 @@
 { pkgs }: {
-	deps = [
-		pkgs.nodejs-18_x
-    pkgs.nodePackages.typescript-language-server
-    pkgs.yarn
-    pkgs.replitPackages.jest
-	];
+    deps = [
+        pkgs.nodejs
+        pkgs.nodePackages.typescript
+        pkgs.ffmpeg
+        pkgs.imagemagick
+        pkgs.git
+        pkgs.neofetch
+        pkgs.libwebp
+        pkgs.speedtest-cli
+        pkgs.wget
+        pkgs.yarn
+        pkgs.libuuid
+    ];
+    env = {
+        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+            pkgs.libuuid
+        ];
+    };
 }
- 
