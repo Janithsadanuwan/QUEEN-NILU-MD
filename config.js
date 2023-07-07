8ef4ef893ecfb926ea98e0afe91701bc984039a6
Queen NIlu <101912503+Dragonxjanith@users.noreply.github.com>
2023-07-07 17:15:08 +0530

    `QUEEN NILU MD 2.0`

--- config.js
+++ config.js
@@ -3,16 +3,17 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env'
 
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
-} 
+}
 
 /*
-TO GET PASSWORD WATCH THIS 
+TO GET PASSWORD WATCH THIS =>
 */
-global.PASSWORD = ''  // Put The Password Get From Yt video  https://youtube.com/
+global.PASSWORD = 'janith'  // Put The Password Get From Yt video  https://youtube.com/
 
 // PUT YOUR SESSION ID HERE 
 
 global.SESSION_ID = 'QueenNilu;;;EjwE0B7b#MynbuRsChyj-dBS2XAO8EFCYZpC1qi4-O4UotLGM3GU' // PUT your session ID BY scan QR 
+
 
 // ⚙️ OWNER SETTINGS  ⚙️
 
@@ -20,38 +21,39 @@ global.OWNER_NUMBER = '94767438882'
 
 global.OWNER_NAME = 'Janiya_X'
 
+global.OPENAI_API_KEY = "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"
+
+
 /*
 ⚙️ BOT  SETTINGS  ⚙️
 */
 
 global.BOT_NAME = 'ǫᴜᴇᴇɴ ɴɪʟᴜ'
+ 
+global.STICKER_NAME = '@Janiya 🤹‍♂️' //sticker
 
-//STIKCER 
-global.STICKER_NAME = '@Janiya 🤹‍♂️' 
-
-global.LANG = 'EN' 
+global.LANG = 'EN' // Language (EN-  TO english  ,  SI- TO sinhala )
 
 global.PREFIX = '.' // Bot Prefix
 
-global.CAPTION = 'ǫᴜᴇᴇɴ ɴɪʟᴜ  © 2023'
+global.CAPTION = 'ǫᴜᴇᴇɴ ɴɪʟᴜ  © 2023' // Caption
 
 global.MAX_SIZE = '10000' // Bot Uloading Max size 
 
-global.ANTI_BAD = 'false'
+global.ANTI_BAD = 'false' // Antibad (on = True , Off = False )
 
-global.ONLY_GROUP = 'false'
+global.ONLY_GROUP = 'false' // Only group mode
 
 global.INBOX_BLOCK = 'off'  // TO Inbox Block On put "on"
 
-global.ANTI_LINK = 'false'
+global.ANTI_LINK = 'false' //Anti Bad (on = True , Off = False )
 
-global.TIMEZONE = 'Asia/Colombo'
+global.TIMEZONE = 'Asia/Colombo' 
 
 global.SEX_DL = 'true' // 18 + video dl
 
-global.GROUP_LINK = 'https://chat.whatsapp.com/EounzhOWL1dIzwryzK4eWx'
+global.GROUP_LINK = 'https://chat.whatsapp.com/' // ur group link
 
-global.OPENAI_API_KEY = "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"
 
 module.exports = {
 
